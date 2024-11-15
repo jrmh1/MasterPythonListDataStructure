5aea320391f218d03f876a2f5235d23762d5047e
jrmh1 <151538560+jrmh1@users.noreply.github.com>
2024-11-15 22:33:37 +0800

    feat:  Add Restaurant class with lists for restaurant details

--- restaurant.py
+++ restaurant.py
@@ -7,6 +7,7 @@ restaurant_established_years = [2011, 2002, 2009, 2013, 2013]
 restaurant_websites_or_contacts = ["www.vikings.ph", "www.antoniosrestaurant.ph", "www.mesa.ph", "www.manam.ph", "www.ramennagi.com.ph"]
 
 
+
 print("Restaurant Data:")
 for i in range(len(restaurant_ids)):
     print(f"ID: {restaurant_ids[i]}, Name: {restaurant_names[i]}, Location: {restaurant_locations[i]}, Cuisine Type: {restaurant_cuisine_types[i]}, Established Year: {restaurant_established_years[i]}, Website/Contact: {restaurant_websites_or_contacts[i]}")
