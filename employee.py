ec5f52962ee143466f7da1abefd7012ee77e9679
jrmh1 <151538560+jrmh1@users.noreply.github.com>
2024-11-15 22:32:51 +0800

    feat: Add Restaurant class with lists for restaurant details

--- employee.py
+++ employee.py
@@ -5,6 +5,7 @@ employee_ages = [30, 25, 40, 28, 35]
 employee_emails = ["bogart.torres@company.com", "bogart.reyes@company.com", "bogart.delacruz@company.com", "lisa.wong@company.com", "paul.mcdonald@company.com"]
 
 
+
 print("Employee Data:")
 for i in range(len(employee_ids)):
     print(f"ID: {employee_ids[i]}, Name: {employee_names[i]}, Department: {employee_departments[i]}, Age: {employee_ages[i]}, Email: {employee_emails[i]}")
