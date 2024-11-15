63e0b2b666b7240db5b4dd371aabb002638cabe1
jrmh1 <151538560+jrmh1@users.noreply.github.com>
2024-11-15 22:34:00 +0800

    feat: Add University class with lists for university details

--- university.py
+++ university.py
@@ -6,7 +6,6 @@ university_established_years = [1908, 1859, 1911, 1611, 1904]
 university_types = ["Public", "Private", "Private", "Private", "Public"]
 university_websites = ["www.up.edu.ph", "www.ateneo.edu", "www.dlsu.edu.ph", "www.ust.edu.ph", "www.pup.edu.ph"]
 
-
 print("University Data:")
 for i in range(len(university_ids)):
     print(f"ID: {university_ids[i]}, Name: {university_names[i]}, Location: {university_locations[i]}, Established Year: {university_established_years[i]}, Type: {university_types[i]}, Website: {university_websites[i]}")
